5dd13205df6f824aa33bd9aa1031ceede03c016b
cuongnd <cuongnd@upstart.vn>
2018-11-20 16:01:30 +0700

    uodate

---
+++ static/js/app/view/BaoCao/KQKTCoSoCoCongSuatDuoi1000m/view/ModelView.js
@@ -0,0 +1,200 @@
+define(function (require) {
+	"use strict";
+	var $                   = require('jquery'),
+	_                   = require('underscore'),
+	Gonrin				= require('gonrin');
+
+	var template 				= require('text!app/view/BaoCao/KQKTCoSoCoCongSuatDuoi1000m/tpl/model.html'),
+	schema 				= require('json!schema/KQKTCoSoCoCongSuatDuoi1000mSchema.json');
+
+	var maxDate = new Date();
+
+	return Gonrin.ModelView.extend({
+		template : template,
+		modelSchema	: schema,
+		urlPrefix: "/api/v1/",
+		collectionName: "kqktcosococongsuatduoi1000m",
+		// uiControl: {
+		// 	fields: [
+		// 		 // {
+		// 		 // field:"ngaybanhanhthongtu",
+		// 		 // textFormat:"DD/MM/YYYY",
+		// 		 // extraFormats:["DDMMYYYY"],
+		// 		 // maxDate,
+		// 		 // },
+
+		// 		{
+		// 			field: "giengdao",
+		// 			uicontrol: "radio",
+		// 			textField: "text",
+		// 			valueField: "value",
+		// 			cssClassField: "cssClass",
+		// 			dataSource: [{
+		// 				value: 0,
+		// 				text: "Có"
+		// 			}, {
+		// 				value: 1,
+		// 				text: "Không",
+		// 				cssClass: "yeallow"
+		// 			},],
+		// 		}, {
+		// 			field: "giengkhoan",
+		// 			uicontrol: "radio",
+		// 			textField: "text",
+		// 			valueField: "value",
+		// 			cssClassField: "cssClass",
+		// 			dataSource: [{
+		// 				value: 0,
+		// 				text: "Có"
+		// 			}, {
+		// 				value: 1,
+		// 				text: "Không",
+		// 				cssClass: "yeallow"
+		// 			},],
+		// 		}, {
+		// 			field: "benuocmua",
+		// 			uicontrol: "radio",
+		// 			textField: "text",
+		// 			valueField: "value",
+		// 			cssClassField: "cssClass",
+		// 			dataSource: [{
+		// 				value: 0,
+		// 				text: "Có"
+		// 			}, {
+		// 				value: 1,
+		// 				text: "Không",
+		// 				cssClass: "yeallow"
+		// 			},],
+		// 		}, {
+		// 			field: "loaikhac",
+		// 			uicontrol: "radio",
+		// 			textField: "text",
+		// 			valueField: "value",
+		// 			cssClassField: "cssClass",
+		// 			dataSource: [{
+		// 				value: 0,
+		// 				text: "Có"
+		// 			}, {
+		// 				value: 1,
+		// 				text: "Không",
+		// 				cssClass: "yeallow"
+		// 			},],
+		// 		}, {
+		// 			field: "cong",
+		// 			uicontrol: "radio",
+		// 			textField: "text",
+		// 			valueField: "value",
+		// 			cssClassField: "cssClass",
+		// 			dataSource: [{
+		// 				value: 0,
+		// 				text: "Có"
+		// 			}, {
+		// 				value: 1,
+		// 				text: "Không",
+		// 				cssClass: "yeallow"
+		// 			},],
+		// 		}, {
+		// 			field: "mangtuchay",
+		// 			uicontrol: "radio",
+		// 			textField: "text",
+		// 			valueField: "value",
+		// 			cssClassField: "cssClass",
+		// 			dataSource: [{
+		// 				value: 0,
+		// 				text: "Có"
+		// 			}, {
+		// 				value: 1,
+		// 				text: "Không",
+		// 				cssClass: "yeallow"
+		// 			},],
+		// 		},
+
+				
+				
+		// 	],
+		// },
+
+
+			tools : [
+			{
+				name: "defaultgr",
+				type: "group",
+				groupClass: "toolbar-group",
+				buttons: [
+				{
+					name: "back",
+					type: "button",
+					buttonClass: "btn-default btn-sm",
+					label: "TRANSLATE:BACK",
+					command: function(){
+						var self = this;
+
+						Backbone.history.history.back();
+					}
+				},
+				{
+					name: "save",
+					type: "button",
+					buttonClass: "btn-success btn-sm",
+					label: "TRANSLATE:SAVE",
+					command: function(){
+						var self = this;
+
+						self.model.save(null,{
+							success: function (model, respose, options) {
+								self.getApp().notify("Lưu thông tin thành công");
+								self.getApp().getRouter().navigate(self.collectionName + "/collection");
+
+							},
+							error: function (model, xhr, options) {
+								self.getApp().notify('Lưu thông tin không thành công!');
+
+							}
+						});
+					}
+				},
+				{
+					name: "delete",
+					type: "button",
+					buttonClass: "btn-danger btn-sm",
+					label: "TRANSLATE:DELETE",
+					visible: function(){
+						return this.getApp().getRouter().getParam("id") !== null;
+					},
+					command: function(){
+						var self = this;
+						self.model.destroy({
+							success: function(model, response) {
+								self.getApp().notify('Xóa dữ liệu thành công');
+								self.getApp().getRouter().navigate(self.collectionName + "/collection");
+							},
+							error: function (model, xhr, options) {
+								self.getApp().notify('Xóa dữ liệu không thành công!');
+
+							}
+						});
+					}
+				},
+				],
+			}],
+			render:function(){
+				var self = this;
+				var id = this.getApp().getRouter().getParam("id");
+				if(id){
+					this.model.set('id',id);
+					this.model.fetch({
+						success: function(data){
+							self.applyBindings();
+						},
+						error:function(){
+							self.getApp().notify("Get data Eror");
+						},
+					});
+				}else{
+					self.applyBindings();
+				}
+
+			},
+		});
+
+});
