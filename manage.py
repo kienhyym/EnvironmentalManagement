7764bc69ad3b8a788f34668f8297e7c0af35e9fe
namdv <dangnam89@gmail.com>
2018-12-26 16:16:55 +0700

    update manage with data template

--- manage.py
+++ manage.py
@@ -88,47 +88,24 @@ def generate_schema(path = None, exclude = None, prettyprint = True):
 
 @manager.command
 def create_quocgia_tinhthanh():
-    quocgias = QuocGia(ma = "VN", ten = "Việt nam")
-    db.session.add(quocgias)
-    db.session.flush() 
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url_tinhthanh = os.path.join(SITE_ROOT, "static/js/app/enum", "TinhThanhEnum.json")
-    data_tinhthanh = json.load(open(json_url_tinhthanh))
-    for item_tinhthanh in data_tinhthanh:
-        print(item_tinhthanh)
-        tinhthanhs = TinhThanh(ma = item_tinhthanh["slug"], ten = item_tinhthanh["name"], quocgia_id = quocgias.id)
-        #tinhthanhs.quocgia.append(quocgias) 
-        db.session.add(tinhthanhs)
-        db.session.commit()
+    
+    db.session.commit()
 
 @manager.command
 def create_dantoc_model():
-<<<<<<< HEAD
     check_exist_dantoc = db.session.query(DanToc).count()
     if (check_exist_dantoc == 0):
-        SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-        json_url_dantoc = os.path.join(SITE_ROOT, "static/js/app/enum", "DanTocEnum.json");
-        data_dantoc = json.load(open(json_url_dantoc))
-        for item_dantoc in data_dantoc:
-            dantoc = DanToc(ma = item_dantoc["value"], ten = item_dantoc["text"])
-            db.session.add(dantoc)
-=======
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url_dantoc = os.path.join(SITE_ROOT, "static/js/app/enum", "DanTocEnum.json")
-    
-    data_dantoc = json.load(open(json_url_dantoc))
-    for item_dantoc in data_dantoc:
-        print(item_dantoc)
-        dantoc = DanToc(ma = item_dantoc["value"], ten = item_dantoc["text"])
-        db.session.add(dantoc)
->>>>>>> e47c4d5c10a807ae57f0c627c5bab318ca36ade6
+        
+            
+            
         db.session.commit()
 
 
 
 #@app.route('/createdata', methods=['GET'])
 @manager.command
-def create_test_models():    
+def create_test_models():  
+    #add tuyen donvi  
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url_tuyendonvi = os.path.join(SITE_ROOT, "static/js/app/enum", "TuyenDonViEnum.json")
     data_tuyendonvi = json.load(open(json_url_tuyendonvi))
@@ -138,10 +115,12 @@ def create_test_models():
         db.session.add(dmTuyenDonVi)
         
     tuyendonvi = TuyenDonVi.query.filter(TuyenDonVi.ma == "TW").first()
-    donvi = DonVi( ten=u'Cục MTYT', captren = None, tuyendonvi_id = tuyendonvi.id)
+    donvi = DonVi( ten=u'Cục quản lý môi trường bộ Y Tế ', captren = None, tuyendonvi_id = tuyendonvi.id)
     db.session.add(donvi)
     db.session.flush()
     
+    
+    #add role
     role1 = Role(id=1,name='Admin')
     db.session.add(role1)
     role2 = Role(id=2,name='CanBo')
@@ -149,16 +128,36 @@ def create_test_models():
     role3 = Role(id=3,name='User')
     db.session.add(role3)
     
-               
-            
+    #add user test     
     user1 = User(email='admin', fullname='Admin', password=auth.encrypt_password('123456'),donvi_id=1,active=True)
     user1.roles.append(role1)
-    user4 = User(email='cuongnd', fullname='Cuong Souciu', password=auth.encrypt_password('123456'),donvi_id=1,active=True)
+    user4 = User(email='cuongnd', fullname='Cuong', password=auth.encrypt_password('123456'),donvi_id=1,active=True)
     user4.roles.append(role1)
     db.session.add(user1)
     user2 = User(email='canbo', fullname='Can Bo', password=auth.encrypt_password('123456'),donvi_id=1,active=True)
     user2.roles.append(role2)
     db.session.add(user2)
+    
+    
+    #add dantoc
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url_dantoc = os.path.join(SITE_ROOT, "static/js/app/enum", "DanTocEnum.json");
+    data_dantoc = json.load(open(json_url_dantoc))
+    for item_dantoc in data_dantoc:
+        dantoc = DanToc(ma = item_dantoc["value"], ten = item_dantoc["text"])
+        db.session.add(dantoc)
+        
+    #add tinhthanh, quoc gia    
+    quocgias = QuocGia(ma = "VN", ten = "Việt nam")
+    db.session.add(quocgias)
+    db.session.flush() 
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url_tinhthanh = os.path.join(SITE_ROOT, "static/js/app/enum", "TinhThanhEnum.json")
+    data_tinhthanh = json.load(open(json_url_tinhthanh))
+    for item_tinhthanh in data_tinhthanh:
+        print(item_tinhthanh)
+        tinhthanhs = TinhThanh(ma = item_tinhthanh["slug"], ten = item_tinhthanh["name"], quocgia_id = quocgias.id)
+        db.session.add(tinhthanhs)
 
          
     db.session.commit()
@@ -167,21 +166,10 @@ def create_test_models():
     
 @manager.command
 def run():
-    quocgia = db.session.query(QuocGia).filter().first()
-    tinhthanhs = db.session.query(TinhThanh).filter().first()
-    if tinhthanhs is None and quocgia is None:
-        create_quocgia_tinhthanh()
-        print("Khoi tao quoc gia , tinh thanh ...")
-
-    dantocs = db.session.query(DanToc).filter().first()
-    if dantocs is None:
-        create_dantoc_model()
-        print("Khoi Tao Dan Toc ....")
-
     role = db.session.query(Role).filter(Role.name == 'User').first()
     if role is None:
         create_test_models()
-        print("Khoi tao admin and role")
+        print("Khoi tao du lieu mau")
         
     run_app(host="0.0.0.0", port=9070)
     
