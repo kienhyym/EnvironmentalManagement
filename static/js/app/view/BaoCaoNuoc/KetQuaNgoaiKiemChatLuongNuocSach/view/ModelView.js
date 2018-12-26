b90d1e2a86469000c6256665cb5e2ab27de22965
CuongNC <cuongnc@gonrin.com>
2018-12-26 21:14:57 +0700

    update ModelView nuoc sach

--- static/js/app/view/BaoCaoNuoc/KetQuaNgoaiKiemChatLuongNuocSach/view/ModelView.js
+++ static/js/app/view/BaoCaoNuoc/KetQuaNgoaiKiemChatLuongNuocSach/view/ModelView.js
@@ -4,11 +4,11 @@ define(function (require) {
 		_ = require('underscore'),
 		Gonrin = require('gonrin');
 
-	var template = require('text!app/view/BaoCaoNuoc/BapCaoNuocSachOne/tpl/model.html'),
-		schema = require('json!schema/BapCaoNuocSachOneSchema.json');
+	var template = require('text!app/view/BaoCaoNuoc/KetQuaNgoaiKiemChatLuongNuocSach/tpl/model.html'),
+		schema = require('json!schema/KetQuaNgoaiKiemChatLuongNuocSachSchema.json');
 
-	var maxDate = new Date();
-	var KQNgoaiKiemChatLuong = require('app/view/BaoCaoNuoc/KQNgoaiKiemChatLuong/view/ModelItemView');
+	//var maxDate = new Date();
+	//var KQNgoaiKiemChatLuong = require('app/view/BaoCaoNuoc/KQNgoaiKiemChatLuong/view/ModelItemView');
 
 	return Gonrin.ModelView.extend({
 		template: template,
@@ -20,109 +20,6 @@ define(function (require) {
 					field: "thoigiankiemtra",
 					textFormat: "DD/MM/YYYY",
 					extraFormats: ["DDMMYYYY"],
-					maxDate,
-				},
-				{
-					field: "kqngoaikiemchatluong",
-					uicontrol: "grid",
-					refresh: true,
-					primaryField: "id",
-					fields: [{
-							field: "vitrilaymau",
-							label: "Vị trí lấy mẫu"
-						},
-						{
-							field: "coliform_ten",
-							label: "Coliform (CFU/100 mL)"
-						},
-						{
-							field: "coli_ten",
-							label: "E.Coli hoặc Coliform chịu nhiệt (CFU/100 mL)"
-						},
-						{
-							field: "aresen_ten",
-							label: "Arsenic (As)(*)mg/L"
-						},
-						{
-							field: "clodu_ten",
-							label: "Clo dư tự do (**) (mg/L)"
-						},
-						{
-							field: "doduc_ten",
-							label: "Độ đục( NTU)"
-						},
-						{
-							field: "mausac_ten",
-							label: "Màu sắc (TCU)"
-						},
-						{
-							field: "muivi_ten",
-							label: "Mùi,vị"
-						},
-						{
-							field: "ph_ten",
-							label: "PH"
-						},
-						{
-							field: "tong_dat",
-							label: "Đánh giá",
-							template: function (rowData) {
-								console.log("rowData====",rowData);
-								if (rowData.ph_dat === 1 || rowData.coliform_dat === 1 || 
-									rowData.coli_dat === 1 || rowData.aresen_dat === 1 || 
-									rowData.clodu_dat === 1 || rowData.mausac_dat === 1|| 
-									rowData.muivi_dat === 1) {
-									return "Đạt";
-
-								} else {
-									return "Không đạt";
-								}
-							}
-						},
-					],
-					tools: [{
-						name: "create",
-						buttonClass: "btn-success",
-						label: "Thêm",
-						command: function () {
-							var self = this;
-							var view = new KQNgoaiKiemChatLuong({
-								"viewData": {
-									"id": null,
-									"baocao_id": self.model.get("id")
-								}
-							});
-							view.dialog();
-							view.on('close', function (data) {
-								var kqngoaikiemchatluong = self.model.get('kqngoaikiemchatluong');
-								kqngoaikiemchatluong.push(data);
-								self.model.set("kqngoaikiemchatluong", kqngoaikiemchatluong);
-								self.applyBindings();
-							});
-						}
-					}],
-					onRowClick: function (event) {
-						var self = this;
-						if (event.rowId) {
-							var view = new KQNgoaiKiemChatLuong({
-								"viewData": {
-									"id": event.rowId,
-									"baocao_id": self.model.get("id")
-								}
-							});
-							view.dialog();
-							view.on('close', function (data) {
-								var str = self.model.get('kqngoaikiemchatluong');
-								for (var i = 0; i < str.length; i++) {
-									if (str[i].id == data.id) {
-										str.splice(i, 1);
-									}
-								}
-								str.push(data);
-								self.applyBindings();
-							});
-						}
-					}
 				},
 			],
 		},
