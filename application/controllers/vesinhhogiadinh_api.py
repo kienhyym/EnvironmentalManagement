ac545ed20dac30408e6d9443602f284112d98a4c
namdv <dangnam89@gmail.com>
2019-01-08 17:47:39 +0700

    update

--- application/controllers/vesinhhogiadinh_api.py
+++ application/controllers/vesinhhogiadinh_api.py
@@ -64,16 +64,43 @@ async def baocao_prepost_vscapthon(request=None, data=None, Model=None, **kw):
     if record is not None:
         return json({"error_code":"PARAMS_ERROR", "error_message":"Báo cáo của đơn vị hiện tại đã được tạo, vui lòng kiểm tra lại"}, status=520)
     
+    data['tenthon'] = data['thonxom']['ten']
     data['tinhtrang'] = TinhTrangBaocaoEnum.taomoi
     data['donvi_id'] = currentuser.donvi_id
     data['nguoibaocao_id'] = currentuser.id
-    data['ngaybaocao'] = datetime.now()
+    data['ngaybaocao'] = str(datetime.now())
+    await process_baocao_vesinh(currentuser,VSCapThon,data)
+    
     
 async def pre_put_vscapthon(request=None, instance_id=None, data=None, **kw):
     currentuser = await current_user(request)
     if currentuser is None:
         return json({"error_code":"SESSION_EXPIRED","error_message":"Hết phiên hoạt động, vui lòng đăng nhập lại"}, status=520)
-      
+    
+    await process_baocao_vesinh(currentuser,VSCapThon,data)
+    
+async def pre_put_vscapxa(request=None, instance_id=None, data=None, **kw):
+    currentuser = await current_user(request)
+    if currentuser is None:
+        return json({"error_code":"SESSION_EXPIRED","error_message":"Hết phiên hoạt động, vui lòng đăng nhập lại"}, status=520)
+    
+    await process_baocao_vesinh(currentuser,VSCapXa,data)
+    
+async def pre_put_vscaphuyen(request=None, instance_id=None, data=None, **kw):
+    currentuser = await current_user(request)
+    if currentuser is None:
+        return json({"error_code":"SESSION_EXPIRED","error_message":"Hết phiên hoạt động, vui lòng đăng nhập lại"}, status=520)
+    
+    await process_baocao_vesinh(currentuser,VSCapHuyen,data)
+
+async def pre_put_vscaptinh(request=None, instance_id=None, data=None, **kw):
+    currentuser = await current_user(request)
+    if currentuser is None:
+        return json({"error_code":"SESSION_EXPIRED","error_message":"Hết phiên hoạt động, vui lòng đăng nhập lại"}, status=520)
+    
+    await process_baocao_vesinh(currentuser,VSCapTinh,data)  
+    
+async def process_baocao_vesinh(currentuser=None, BaoCao=None, data=None):
     kybaocaotruoc = 1
     nambaocao_truoc = None
     if (data['kybaocao'] > 1):
@@ -88,55 +115,68 @@ async def pre_put_vscapthon(request=None, instance_id=None, data=None, **kw):
         elif (data['loaikybaocao'] == LoaiKyBaoCao.SAUTHANG):
             kybaocaotruoc = 2
             
-    baocaokytruoc = db.session.query(VSCapThon).filter(and_(VSCapThon.donvi_id == currentuser.donvi_id,\
-                                                      VSCapThon.thonxom_id == data['thonxom_id'], \
-                                                      VSCapThon.loaikybaocao == data['loaikybaocao'], \
-                                                      VSCapThon.kybaocao == kybaocaotruoc, \
-                                                      VSCapThon.nambaocao == nambaocao_truoc)).first()    
+    baocaokytruoc = db.session.query(BaoCao).filter(and_(BaoCao.donvi_id == currentuser.donvi_id,\
+#                                                       VSCapThon.thonxom_id == data['thonxom_id'], \
+                                                      BaoCao.loaikybaocao == data['loaikybaocao'], \
+                                                      BaoCao.kybaocao == kybaocaotruoc, \
+                                                      BaoCao.nambaocao == nambaocao_truoc)).first()    
     
     if baocaokytruoc is not None:
-        data["tongho_conhatieu_truocbaocao"] = baocaokytruoc.tong_soho - baocaokytruoc.tong_khongnhatieu
-        data["tongho_conhatieu_hvs_truocbaocao"] = baocaokytruoc.tong_hopvs
-        data["tong_soho_conhatieu_tuhoai_truocbaocao"] = baocaokytruoc.tong_tuhoai
-        data["tong_soho_conhatieu_thamdoi_truocbaocao"] = baocaokytruoc.tong_thamdoi
-        data["tong_soho_conhatieu_2ngan_hvs_truocbaocao"] = baocaokytruoc.tong_2ngan
-#         data["tong_soho_conhatieu_vip_hvs_truocbaocao"] = baocaokytruoc.tong_vip
-        data["tong_soho_conhatieu_caithien_truocbaocao"] = baocaokytruoc.tong_caithien
-        data["tong_hongheo_conhatieu_caithien_truocbaocao"] = baocaokytruoc.tong_hongheo_caithien
+        data["tong_soho_conhatieu_truocbaocao"] = baocaokytruoc.tong_soho - baocaokytruoc.tong_khongnhatieu
+        data["tong_soho_conhatieu_hvs_truocbaocao"] = baocaokytruoc.tong_hopvs
+        data["tong_soho_conhatieu_tuhoai_hvs_truocbaocao"] = baocaokytruoc.tong_tuhoai_hvs
+        data["tong_soho_conhatieu_thamdoi_hvs_truocbaocao"] = baocaokytruoc.tong_thamdoi_hvs
+        data["tong_soho_conhatieu_2ngan_hvs_truocbaocao"] = baocaokytruoc.tong_2ngan_hvs
+        data["tong_soho_conhatieu_vip_hvs_truocbaocao"] = baocaokytruoc.tong_ongthonghoi_hvs
+        data["tong_soho_conhatieu_caithien_hvs_truocbaocao"] = baocaokytruoc.tong_caithien_hvs
+        data["tong_soho_conhatieu_caithien_hongheo_hvs_truocbaocao"] = baocaokytruoc.tong_caithien_hongheo_hvs
         
         nhatieuthon_kytruoc  = baocaokytruoc.nhatieuthonhvs
         nhatieuthon_kyhientai = data["nhatieuthonhvs"]
-        tongho_conhatieu_hvs_xuongcap = 0
-        tong_soho_conhatieu_tuhoai_truocbaocao = 0
-        tong_soho_conhatieu_thamdoi_xuongcap = 0
+        tong_soho_conhatieu_hvs_xuongcap = 0
+        tong_soho_conhatieu_tuhoai_hvs_xuongcap = 0
+        tong_soho_conhatieu_thamdoi_hvs_xuongcap = 0
         tong_soho_conhatieu_2ngan_hvs_xuongcap = 0
-        tong_soho_conhatieu_caithien_xuongcap = 0
-        tong_hongheo_conhatieu_caithien_xuongcap = 0
-        tong_soho_conhatieu_tuhoai_xuongcap = 0
+        tong_soho_conhatieu_vip_hvs_xuongcap = 0
+        tong_soho_conhatieu_caithien_hvs_xuongcap = 0
+        tong_soho_conhatieu_caithien_hongheo_hvs_xuongcap = 0
         for nt_truoc in nhatieuthon_kytruoc:
             for nt_sau in nhatieuthon_kyhientai:
                 if nt_truoc["maho"] == nt_sau["maho"]:
                     if(nt_truoc["hopvesinh"] == 1 and (nt_sau["hopvesinh"] is None or nt_sau["hopvesinh"] == 0)):
-                        tongho_conhatieu_hvs_xuongcap = tongho_conhatieu_hvs_xuongcap +1
-                    if(nt_truoc["tuhoai"] == 1 and (nt_sau["tuhoai"] is None or nt_sau["tuhoai"] == 0)):
-                        tong_soho_conhatieu_tuhoai_xuongcap = tong_soho_conhatieu_tuhoai_xuongcap +1
-                    if(nt_truoc["thamdoi"] == 1 and (nt_sau["thamdoi"] is None or nt_sau["thamdoi"]== 0)):
-                        tong_soho_conhatieu_thamdoi_xuongcap = tong_soho_conhatieu_thamdoi_xuongcap +1
-                    if(nt_truoc["haingan"] == 1 and (nt_sau["haingan"] is None or nt_sau["haingan"] == 0)):
-                        tong_soho_conhatieu_2ngan_hvs_xuongcap = tong_soho_conhatieu_2ngan_hvs_xuongcap +1
-                    if(nt_truoc["caithien"] == 1 and (nt_sau["caithien"] is None or nt_sau["caithien"] == 0)):
-                        tong_soho_conhatieu_caithien_xuongcap = tong_soho_conhatieu_caithien_xuongcap +1
+                        tong_soho_conhatieu_hvs_xuongcap += 1
+                    
+                    if((nt_truoc["tuhoai"] == 1 and nt_truoc["hopvesinh"] == 1) and (nt_sau["tuhoai"] is None or nt_sau["tuhoai"] == 0 or nt_sau["hopvesinh"] is None or nt_sau["hopvesinh"] == 0)):
+                        tong_soho_conhatieu_tuhoai_hvs_xuongcap += 1
+                    
+                    if((nt_truoc["thamdoi"] == 1 and nt_truoc["hopvesinh"] == 1) and (nt_sau["thamdoi"] is None or nt_sau["thamdoi"]== 0  or nt_sau["hopvesinh"] is None or nt_sau["hopvesinh"] == 0)):
+                        tong_soho_conhatieu_thamdoi_hvs_xuongcap += 1
+                    
+                    if((nt_truoc["haingan"] == 1 and nt_truoc["hopvesinh"] == 1) and (nt_sau["haingan"] is None or nt_sau["haingan"] == 0 or nt_sau["hopvesinh"] is None or nt_sau["hopvesinh"] == 0)):
+                        tong_soho_conhatieu_2ngan_hvs_xuongcap += 1
+                        
+                    if((nt_truoc["chimco_oth"] == 1 and nt_truoc["hopvesinh"] == 1 ) and (nt_sau["chimco_oth"] is None or nt_sau["chimco_oth"] == 0 or nt_sau["hopvesinh"] is None or nt_sau["hopvesinh"] == 0)):
+                        tong_soho_conhatieu_vip_hvs_xuongcap += 1
+                        
+                    if((nt_truoc["caithien"] == 1 and nt_truoc["hopvesinh"] == 1) and (nt_sau["caithien"] is None or nt_sau["caithien"] == 0 or nt_sau["hopvesinh"] is None or nt_sau["hopvesinh"] == 0)):
+                        tong_soho_conhatieu_caithien_hvs_xuongcap += 1
                         if(nt_truoc["hongheo"] == 1 and nt_sau["hongheo"] == 1):
-                            tong_hongheo_conhatieu_caithien_xuongcap = tong_hongheo_conhatieu_caithien_xuongcap +1
-                          
-        data["tongho_conhatieu_hvs_xuongcap"] = tongho_conhatieu_hvs_xuongcap
-        data["tong_soho_conhatieu_tuhoai_xuongcap"] = tong_soho_conhatieu_tuhoai_xuongcap
-        data["tong_soho_conhatieu_thamdoi_xuongcap"] = tong_soho_conhatieu_thamdoi_xuongcap
+                            tong_soho_conhatieu_caithien_hongheo_hvs_xuongcap += 1
+        tong_soho_conhatieu_hvs_xuongcap = 0
+        tong_soho_conhatieu_tuhoai_hvs_xuongcap = 0
+        tong_soho_conhatieu_thamdoi_hvs_xuongcap = 0
+        tong_soho_conhatieu_2ngan_hvs_xuongcap = 0
+        tong_soho_conhatieu_vip_hvs_xuongcap = 0
+        tong_soho_conhatieu_caithien_hvs_xuongcap = 0
+        tong_soho_conhatieu_caithien_hongheo_hvs_xuongcap = 0                  
+        data["tong_soho_conhatieu_hvs_xuongcap"] = tong_soho_conhatieu_hvs_xuongcap
+        data["tong_soho_conhatieu_tuhoai_hvs_xuongcap"] = tong_soho_conhatieu_tuhoai_hvs_xuongcap
+        data["tong_soho_conhatieu_thamdoi_hvs_xuongcap"] = tong_soho_conhatieu_thamdoi_hvs_xuongcap
         data["tong_soho_conhatieu_2ngan_hvs_xuongcap"] = tong_soho_conhatieu_2ngan_hvs_xuongcap
-        data["tong_soho_conhatieu_caithien_xuongcap"] = tong_soho_conhatieu_caithien_xuongcap
-        data["tong_hongheo_conhatieu_caithien_xuongcap"] = tong_hongheo_conhatieu_caithien_xuongcap
+        data["tong_soho_conhatieu_vip_hvs_xuongcap"] = tong_soho_conhatieu_vip_hvs_xuongcap
+        data["tong_soho_conhatieu_caithien_hvs_xuongcap"] = tong_soho_conhatieu_caithien_hvs_xuongcap
+        data["tong_soho_conhatieu_caithien_hongheo_hvs_xuongcap"] = tong_soho_conhatieu_caithien_hongheo_hvs_xuongcap
         
-    data['tenthon'] = data['thonxom']['ten']
     
 async def baocao_prepost_vscapxa(request=None, data=None, Model=None, **kw):
     currentuser = await current_user(request)
@@ -160,7 +200,8 @@ async def baocao_prepost_vscapxa(request=None, data=None, Model=None, **kw):
     data['tinhtrang'] = TinhTrangBaocaoEnum.taomoi
     data['donvi_id'] = currentuser.donvi_id
     data['nguoibaocao_id'] = currentuser.id
-    data['ngaybaocao'] = datetime.now()
+    data['ngaybaocao'] = str(datetime.now())
+    await process_baocao_vesinh(currentuser,VSCapXa,data)
     
 async def baocao_prepost_vscaphuyen(request=None, data=None, Model=None, **kw):
     currentuser = await current_user(request)
@@ -183,7 +224,8 @@ async def baocao_prepost_vscaphuyen(request=None, data=None, Model=None, **kw):
     data['tinhtrang'] = TinhTrangBaocaoEnum.taomoi
     data['donvi_id'] = currentuser.donvi_id
     data['nguoibaocao_id'] = currentuser.id
-    data['ngaybaocao'] = datetime.now()
+    data['ngaybaocao'] = str(datetime.now())
+    await process_baocao_vesinh(currentuser,VSCapHuyen,data)
     
 async def baocao_prepost_vscaptinh(request=None, data=None, Model=None, **kw):
     currentuser = await current_user(request)
@@ -206,7 +248,8 @@ async def baocao_prepost_vscaptinh(request=None, data=None, Model=None, **kw):
     data['tinhtrang'] = TinhTrangBaocaoEnum.taomoi
     data['donvi_id'] = currentuser.donvi_id
     data['nguoibaocao_id'] = currentuser.id
-    data['ngaybaocao'] = datetime.now()
+    data['ngaybaocao'] = str(datetime.now())
+    await process_baocao_vesinh(currentuser,VSCapTinh,data)
 
 async def reponse_capxa_get_single(request=None, Model=None, result=None, **kw):
     currentuser = await current_user(request)
@@ -254,7 +297,7 @@ apimanager.create_api(HoGiaDinh,
 apimanager.create_api(VSCapThon,
     methods=['GET', 'POST', 'DELETE', 'PUT'],
     url_prefix='/api/v1',
-    preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func,entity_pregetmany], POST=[auth_func, baocao_prepost_vscapthon, pre_put_vscapthon], PUT_SINGLE=[auth_func, pre_put_vscapthon], DELETE_SINGLE=[auth_func]),
+    preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func,entity_pregetmany], POST=[auth_func, baocao_prepost_vscapthon], PUT_SINGLE=[auth_func, pre_put_vscapthon], DELETE_SINGLE=[auth_func]),
     postprocess=dict(POST=[], PUT_SINGLE=[], DELETE_SINGLE=[]),
     collection_name='vscapthon')
 
@@ -267,7 +310,7 @@ apimanager.create_api(VSCapThon,
 apimanager.create_api(VSCapXa,
     methods=['GET', 'POST', 'DELETE', 'PUT'],
     url_prefix='/api/v1',
-    preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func, entity_pregetmany], POST=[auth_func, baocao_prepost_vscapxa], PUT_SINGLE=[auth_func], DELETE_SINGLE=[auth_func]),
+    preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func, entity_pregetmany], POST=[auth_func, baocao_prepost_vscapxa], PUT_SINGLE=[auth_func, pre_put_vscapxa], DELETE_SINGLE=[auth_func]),
     postprocess=dict(GET_SINGLE=[reponse_capxa_get_single], PUT_SINGLE=[], DELETE_SINGLE=[]),
     collection_name='vscapxa')
 
@@ -275,7 +318,7 @@ apimanager.create_api(VSCapXa,
 apimanager.create_api(VSCapHuyen,
     methods=['GET', 'POST', 'DELETE', 'PUT'],
     url_prefix='/api/v1',
-    preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func, entity_pregetmany], POST=[auth_func, baocao_prepost_vscaphuyen], PUT_SINGLE=[auth_func], DELETE_SINGLE=[auth_func]),
+    preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func, entity_pregetmany], POST=[auth_func, baocao_prepost_vscaphuyen], PUT_SINGLE=[auth_func,pre_put_vscaphuyen], DELETE_SINGLE=[auth_func]),
     postprocess=dict(GET_SINGLE=[reponse_caphuyen_get_single], PUT_SINGLE=[], DELETE_SINGLE=[]),
     collection_name='vscaphuyen')
 
@@ -283,7 +326,7 @@ apimanager.create_api(VSCapHuyen,
 apimanager.create_api(VSCapTinh,
     methods=['GET', 'POST', 'DELETE', 'PUT'],
     url_prefix='/api/v1',
-    preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func, entity_pregetmany], POST=[auth_func, baocao_prepost_vscaptinh], PUT_SINGLE=[auth_func], DELETE_SINGLE=[auth_func]),
+    preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func, entity_pregetmany], POST=[auth_func, baocao_prepost_vscaptinh], PUT_SINGLE=[auth_func,pre_put_vscaptinh], DELETE_SINGLE=[auth_func]),
     postprocess=dict(GET_SINGLE=[reponse_captinh_get_single], PUT_SINGLE=[], DELETE_SINGLE=[]),
     collection_name='vscaptinh')
 
