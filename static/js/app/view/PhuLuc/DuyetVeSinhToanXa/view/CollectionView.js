91453ffae3525261fbb61565a409ec70f324196d
hauntq98 <hauntq98@gmail.com>
2019-01-11 15:03:07 +0700

    upload hautc

--- static/js/app/view/PhuLuc/DuyetVeSinhToanXa/view/CollectionView.js
+++ static/js/app/view/PhuLuc/DuyetVeSinhToanXa/view/CollectionView.js
@@ -14,11 +14,32 @@ define(function (require) {
     	collectionName: "duyet_vesinh_toanxa",
     	uiControl:{
     		fields: [
-	    	     { 
-	    	    	field: "mathongso",label:"Mã Thông Số",
-	    	     },
-	    	     { field: "tenthongso", label: "Tên Thông số"},
-	    	     { field: "gioihan_toida_txt", label: "Giới hạn tối đa"},
+    			{
+					field: "tinhthanh_id",
+					label: "Tỉnh",
+					foreign: "tinhthanh",
+					foreignValueField: "id",
+					foreignTextField: "ten",
+					width: 250
+				},
+				{
+					field: "quanhuyen_id",
+					label: "Huyện",
+					foreign: "quanhuyen",
+					foreignValueField: "id",
+					foreignTextField: "ten",
+					width: 250
+				},
+				{
+					field: "xaphuong_id",
+					label: "Xã",
+					foreign: "xaphuong",
+					foreignValueField: "id",
+					foreignTextField: "ten",
+					width: 250
+				},
+	    	     { field: "nam_datvesinh_toanxa", label: "Năm đạt vệ sinh toàn xã"},
+	    	     { field: "nam_datvesinh_toanxa_benvung", label: "Năm đạt vệ sinh toàn xã bền vững"},
 		     ],
 		     onRowClick: function(event){
 		    	if(event.rowId){
