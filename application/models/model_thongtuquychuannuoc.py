23a74ead3018263f3105796a56a2bafb1ca6a9df
namdv <dangnam89@gmail.com>
2019-01-15 10:45:10 +0700

    update model nuoc

--- application/models/model_thongtuquychuannuoc.py
+++ application/models/model_thongtuquychuannuoc.py
@@ -3,7 +3,7 @@ import uuid
 from application.database import db
 from application.database.model import CommonModel
 
-from sqlalchemy import (DECIMAL, Boolean, Column, Date, DateTime, Float,ForeignKey, Integer, String, Text,JSON, UniqueConstraint)
+from sqlalchemy import (DECIMAL, Boolean, Column, Date, DateTime, Float,ForeignKey, Integer,SmallInteger, String, Text,JSON, UniqueConstraint)
 from sqlalchemy.dialects.postgresql import  UUID, JSONB
 from sqlalchemy.orm import *
 from sqlalchemy.orm import backref, relationship
@@ -11,6 +11,14 @@ from sqlalchemy.orm.collections import attribute_mapped_collection
 
 def default_uuid():
     return str(uuid.uuid4())
+
+class MapVienChuyenNganhNuocVaTinh(CommonModel):
+    __tablename__ = 'map_vienchuyennganhnuoc_tinh'
+    donvi_id = db.Column(db.Integer, db.ForeignKey('donvi.id'), nullable=False)
+    donvi = db.relationship('DonVi', viewonly=True)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=True)
+    user = db.relationship('User', viewonly=True)
+    danhsachtinhthanh = db.Column(JSONB)
 
 #Danh muc cac don vi cap nuoc
 class DonViCapNuoc(CommonModel):
@@ -20,7 +28,9 @@ class DonViCapNuoc(CommonModel):
     diachi = db.Column(db.String)
     congsuat = db.Column(DECIMAL)
     tongso_hogiadinh = db.Column(db.Integer)
-    nguonnuoc = db.Column(db.String)
+    tansuat_noikiem = db.Column(db.Integer)
+    nguonnuoc_nguyenlieu = db.Column(db.String)
+    phuongphap_khutrung = db.Column(db.SmallInteger)
     
     tinhthanh_id = db.Column(UUID(as_uuid=True), ForeignKey('tinhthanh.id'), nullable=True)
     tinhthanh = relationship('TinhThanh')
@@ -28,6 +38,52 @@ class DonViCapNuoc(CommonModel):
     quanhuyen = relationship('QuanHuyen')
     xaphuong_id = db.Column(UUID(as_uuid=True), ForeignKey('xaphuong.id'), nullable=True)
     xaphuong = relationship('XaPhuong')
+
+## Mau so 5TT - Bao cao so 1: Bao Cao Kết quả nội kiểm chất lượng nước sạch    
+class KetQuaNoiKiemChatLuongNuocSach(CommonModel):
+    __tablename__ = 'ketqua_noikiem_chatluong_nuocsach'
+    donvi_id = db.Column(db.Integer, db.ForeignKey('donvi.id'), nullable=False)
+    donvi = db.relationship('DonVi', viewonly=True)
+    nguoibaocao_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=True)
+    nguoibaocao = db.relationship('User', viewonly=True)
+    tinhtrang = db.Column(db.SmallInteger,nullable=False)
+    nambaocao = db.Column(db.Integer, nullable=False)
+    ngaybaocao = db.Column(db.DateTime())
+    donvicapnuoc_id = db.Column(UUID(as_uuid=True), db.ForeignKey('donvicapnuoc.id'), nullable=False)
+    donvicapnuoc = db.relationship('DonViCapNuoc', viewonly=True)
+    tendonvicapnuoc = db.Column(db.String)
+    diachi_donvicapnuoc = db.Column(db.String)
+    congsuat_thietke = db.Column(DECIMAL)
+    tongso_hogiadinh = db.Column(db.Integer)
+    nguonnuoc_nguyenlieu = db.Column(db.String)
+    tansuat_noikiem = db.Column(db.String)
+    thoigiankiemtra = db.Column(db.DateTime())
+    nguoikiemtra = db.Column(db.String)
+    
+    laphoso_theoquydinh = db.Column(db.SmallInteger)
+    hoso_daydu_theoquydinh = db.Column(db.SmallInteger)
+    tailieu_thieu = db.Column(db.String)
+    somau_thunghiem_dungquydinh = db.Column(db.SmallInteger)
+    thunghiem_daydu_thongso = db.Column(db.SmallInteger)
+    tansuat_thuchien_noikiem_dungquydinh = db.Column(db.SmallInteger)
+    thuchien_baocao_daydu = db.Column(db.SmallInteger)
+    thuchien_congkhai_thongtin = db.Column(db.SmallInteger)
+    thuchien_bienphap_khacphuc = db.Column(db.SmallInteger)
+    
+    somauvavitri = db.Column(db.Integer)
+    danhsachvitrilaymau = db.Column(JSONB)
+    
+    ketquanoikiemchatluongnuoc = db.Column(JSONB)
+    ketquanoikiem = db.Column(db.String)
+    nhanxet = db.Column(db.String)
+    
+    cothuchien_khacphuc = db.Column(db.SmallInteger)
+    bienphap_khacphuc = db.Column(db.String)
+    
+    
+    kiennghi = db.Column(db.String)
+    ketluan = db.Column(db.String)
+
 
 ##MauSo 1 ket qua ngoai kiem nuoc sach
 class KetQuaNgoaiKiemChatLuongNuocSach(CommonModel):
@@ -37,34 +93,58 @@ class KetQuaNgoaiKiemChatLuongNuocSach(CommonModel):
     nguoibaocao_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=True)
     nguoibaocao = db.relationship('User', viewonly=True)
     tinhtrang = db.Column(db.SmallInteger,nullable=False)
-#     ngaybaocao = db.Column(db.DateTime())
     nambaocao = db.Column(db.Integer, nullable=False)
     ngaybaocao = db.Column(db.DateTime())
     donvicapnuoc_id = db.Column(UUID(as_uuid=True), db.ForeignKey('donvicapnuoc.id'), nullable=False)
     donvicapnuoc = db.relationship('DonViCapNuoc', viewonly=True)
     tendonvicapnuoc = db.Column(db.String)
-    madonvicapnuoc = db.Column(UUID(as_uuid=True))
     diachi_donvicapnuoc = db.Column(db.String)
-    congxuat = db.Column(DECIMAL)
+    congsuat_thietke = db.Column(DECIMAL)
     tongso_hogiadinh = db.Column(db.Integer)
-    nguonnuoc = db.Column(db.String)
-    
+    nguonnuoc_nguyenlieu = db.Column(db.String)
+    tansuat_noikiem = db.Column(db.String)
     thoigiankiemtra = db.Column(db.DateTime())
     
+    tendonvi_ngoaikiem = db.Column(db.String)
+    noidung_ngoaikiem = db.Column(db.String)
+    thunghiem_chatluong_nuoc = db.Column(db.SmallInteger)
+    loai_donvi_kiemtra = db.Column(db.SmallInteger)
     thanhphan_doankiemtra = db.Column(db.String)
     
+    laphoso_theoquydinh = db.Column(db.SmallInteger)
+    hoso_daydu_theoquydinh = db.Column(db.SmallInteger)
+    tailieu_thieu = db.Column(db.String)
+    somau_thunghiem_dungquydinh = db.Column(db.SmallInteger)
+    thunghiem_daydu_thongso = db.Column(db.SmallInteger)
+    tansuat_thuchien_noikiem_dungquydinh = db.Column(db.SmallInteger)
+    thuchien_baocao_daydu = db.Column(db.SmallInteger)
+    thuchien_congkhai_thongtin = db.Column(db.SmallInteger)
+    thuchien_bienphap_khacphuc = db.Column(db.SmallInteger)
+    
+    tongsomau_thunghiem = db.Column(db.Integer)
+    tongsomau_dat_quychuan = db.Column(db.Integer)
+    tongsomau_khongdat_quychuan = db.Column(db.Integer)
+    
+    tong_thongso_khongdat = db.Column(db.Integer)
+    danhsachthongso_khongdat = db.Column(JSONB)
+                                      
     somauvavitri = db.Column(db.Integer)
     danhsachvitrilaymau = db.Column(JSONB)
     
-    hosotheodoi = db.Column(db.String)
-    tansuatthuchien_chedonoikiem = db.Column(db.String)
-    tinhhinhchatluongnuoc = db.Column(db.String)
-    thuchien_chedo = db.Column(db.String)
-    nhanxet = db.Column(db.String)
-    kiennghi = db.Column(db.String)
-    ketluan = db.Column(db.String)
     ketquangoaikiemchatluongnuoc = db.Column(JSONB)
     ketquangoaikiem = db.Column(db.String)
+    nhanxet = db.Column(db.String)
+    
+    congbo_thongtin_chodonvicapnuoc = db.Column(db.SmallInteger)
+    congkhai_thongtin = db.Column(db.SmallInteger)
+    thongbao_coquan_thamquyen = db.Column(db.SmallInteger)
+    thongbao_donvi_chuquan = db.Column(db.SmallInteger)
+    
+    kiennghi = db.Column(db.String)
+    ketluan = db.Column(db.String)
+    
+    
+    
     
 class ThongSoBaoCaoChatLuongNuoc(CommonModel):
     __tablename__ = 'thongsobaocaochatluongnuoc'
@@ -77,43 +157,9 @@ class ThongSoBaoCaoChatLuongNuoc(CommonModel):
     batbuoc = db.Column(db.Boolean)
     baocaoapdung = db.Column(JSONB)
 
-## Mau so 5: Bao Cao Kết quả nội kiểm chất lượng nước sạch    
-class KetQuaNoiKiemChatLuongNuocSach(CommonModel):
-    __tablename__ = 'ketqua_noikiem_chatluong_nuocsach'
-    donvi_id = db.Column(db.Integer, db.ForeignKey('donvi.id'), nullable=False)
-    donvi = db.relationship('DonVi', viewonly=True)
-    nguoibaocao_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=True)
-    nguoibaocao = db.relationship('User', viewonly=True)
-    tinhtrang = db.Column(db.SmallInteger,nullable=False)
-#     ngaybaocao = db.Column(db.DateTime())
-    nambaocao = db.Column(db.Integer, nullable=False)
+
     
-    ngaybaocao = db.Column(db.DateTime())
-    donvicapnuoc_id = db.Column(UUID(as_uuid=True), db.ForeignKey('donvicapnuoc.id'), nullable=False)
-    donvicapnuoc = db.relationship('DonViCapNuoc', viewonly=True)
-    tendonvicapnuoc = db.Column(db.String)
-    madonvicapnuoc = db.Column(UUID(as_uuid=True))
-    diachi_donvicapnuoc = db.Column(db.String)
-    congxuat = db.Column(DECIMAL)
-    tongso_hogiadinh = db.Column(db.Integer)
-    nguonnuoc = db.Column(db.String)
     
-    thoigiankiemtra = db.Column(db.DateTime())
-    
-    nguoikiemtra = db.Column(db.String)
-    
-    somauvavitri = db.Column(db.Integer)
-    danhsachvitrilaymau = db.Column(JSONB)
-    
-    hosotheodoi = db.Column(db.String)
-#     tansuatthuchien_chedonoikiem = db.Column(db.String)
-#     tinhhinhchatluongnuoc = db.Column(db.String)
-#     thuchien_chedo = db.Column(db.String)
-    nhanxet = db.Column(db.String)
-    bienphapkhacphuc = db.Column(db.String)
-    denghi = db.Column(db.String)
-    ketquanoikiemchatluongnuoc = db.Column(JSONB)
-    ketquanoikiem = db.Column(db.String)
 
 ##Mau so 2
 class KetQuaKiemTraChatLuongNuocSach(CommonModel):
