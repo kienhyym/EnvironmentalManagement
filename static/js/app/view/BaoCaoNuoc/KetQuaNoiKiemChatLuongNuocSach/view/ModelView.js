a9d87723b1174f9addea2e08b94f81bb877fde5c
hauntq98 <hauntq98@gmail.com>
2019-03-29 10:01:25 +0700

    upload hautc

--- static/js/app/view/BaoCaoNuoc/KetQuaNoiKiemChatLuongNuocSach/view/ModelView.js
+++ static/js/app/view/BaoCaoNuoc/KetQuaNoiKiemChatLuongNuocSach/view/ModelView.js
@@ -238,6 +238,20 @@ define(function (require) {
                 }
             },
             {
+				name: "export_pdf",
+				type: "button",
+				buttonClass: "btn-warning btn-sm",
+				label: "TRANSLATE:EXPORT_PDF",
+				visible: function () {
+					return this.getApp().getRouter().getParam("id") !== null;
+				},
+				command: function () {
+					var self = this;
+					var filename = "baocao_"+self.model.get("tendonvicapnuoc")+"_"+self.model.get("nambaocao");
+					self.getApp().exportPDF_HTML2PDF("content",filename);
+				}
+			},
+            {
                 name: "delete",
                 type: "button",
                 buttonClass: "btn-danger btn-sm",
