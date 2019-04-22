2cb4f32536db1b9dd1e0131c21384d81d81133c8
namdv <dangnam89@gmail.com>
2019-04-23 00:02:35 +0700

    update

--- application/__init__.py
+++ application/__init__.py
@@ -3,4 +3,4 @@ from .server import app
 
 def run_app(host="127.0.0.1", port=9070, debug=False):
     """ Start project MoiTruong YTE with port 9070. """
-    app.run(host=host, port=port, debug=debug, workers=1)
+    app.run(host=host, port=port, debug=debug, workers=3)
