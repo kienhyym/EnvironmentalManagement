d720f706df9dc5e7b8de50123086aa7f3bd8e0b0
namdv <dangnam89@gmail.com>
2019-04-26 06:40:02 +0700

    update viennuocsach

--- static/js/app/view/BaoCaoNuoc/BaoCaoVienChuyenNganhNuoc/view/CollectionView.js
+++ static/js/app/view/BaoCaoNuoc/BaoCaoVienChuyenNganhNuoc/view/CollectionView.js
@@ -93,16 +93,6 @@ define(function (require) {
 				});
 				filter.render();
 				 
-				if(!filter.isEmptyFilter()) {
-					var $col = self.getCollectionElement();
-					var text = !!filter.model.get("text") ? filter.model.get("text").trim() : "";
-					var filters = {"$and":[
-							{"nambaocao": {"$eq": text}},
-							{"loaikybaocao":{"$eq":itemkybaocao.loaikybaocao}}, 
-							{"kybaocao":{"$eq":itemkybaocao.kybaocao}},
-							{"donvi_id":{"$eq":self.getApp().currentUser.donvi_id}}]};
-					$col.data('gonrin').filter(filters);
-				}
 				self.applyBindings();
 	
 				filter.on('filterChanged', function(evt) {
